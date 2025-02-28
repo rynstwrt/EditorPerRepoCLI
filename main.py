94dfa6d775b843b866bb29e513a5e03d72b3221f
Ryn. <rynstwrt17@gmail.com>
2025-02-27 22:24:18 -0600

    Add automatic editor type detection

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
+from operator import itemgetter
 from pathlib import Path
 import sys
+import glob
 import subprocess
+import json
 
 
-CONFIG_FILE_NAME = ".repo-editor"
+EDITOR_FILE_TYPE_CONFIG = "editor-file-types.json"
+
+
+def detect_editor_from_file_types(repo_dir):
+    file_content = open(EDITOR_FILE_TYPE_CONFIG, "r")
+    data = json.load(file_content)
+    print(data)
+
+    extensions = data.keys()
+    most_common_file_type = None
+    for extension in extensions:
+        files_of_type = repo_dir.glob(f"*{extension}")
+        num_files = len(list(files_of_type))
+        if not most_common_file_type or most_common_file_type[1] < num_files:
+            most_common_file_type = (extension, num_files)
+
+    file_type, num_files = most_common_file_type
+    print(f"Most common file extension is {file_type} with {num_files} files found.")
+
+    editor_info = data[file_type]
+    print(editor_info)
+
+    parent_dir = editor_info["parentDir"]
+    exec_name = editor_info["executableName"]
+    print(parent_dir, exec_name)
+
+    parent_dir_path = Path(parent_dir)
+    exec_path = list(parent_dir_path.glob(f"**/{exec_name}"))
+    print(f"Found executable path at {exec_path}!")
+
+    return exec_path
 
 
 def main(repo_dir):
@@ -14,17 +47,10 @@ def main(repo_dir):
         print("Error: Given repo directory does not exist!")
         return
 
-    editor_config_file = repo_dir.joinpath(CONFIG_FILE_NAME)
-    print(editor_config_file)
-    if not editor_config_file.exists():
-        return print(f"Error: {CONFIG_FILE_NAME} file was not found in that repo!")
+    detected_editor = detect_editor_from_file_types(repo_dir)
+    print(detected_editor)
 
-    editor_command = open(editor_config_file).read()
-    print(editor_command)
-    if not editor_command:
-        return print("Error: Config data could not be read!")
-
-    subprocess.call([editor_command, repo_dir])
+    subprocess.call([detected_editor[0], repo_dir])
 
 
 if __name__ == "__main__":
